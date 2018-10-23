81fe33dff381cbbe6b43414af96c75c916f96714
kreichjr <43842580+kreichjr@users.noreply.github.com>
2018-10-22 23:35:39 -0500

    Add files via upload

--- reaper_control_client.py
+++ reaper_control_client.py
@@ -1,20 +1,95 @@
 # Client script to send data to the server to run commands to 
 # control my DAW
 
+import tkinter as tk
 import socket
 import sys
 
 HOST = '127.0.0.1'    # The remote host
 PORT = 6666              # The same port as used by the server
 
+def stop_button_cmd(socket_src):
+    cmd = "stop"
+    socket_src.sendall(cmd.strip().encode())
+
+def play_button_cmd(socket_src):
+    cmd = "play"
+    socket_src.sendall(cmd.strip().encode())
+
+def record_button_cmd(socket_src):
+    cmd = "record"
+    socket_src.sendall(cmd.strip().encode())
+
+def quit_button_cmd(socket_src):
+    cmd = "endme"
+    socket_src.sendall(cmd.strip().encode())
+    sys.exit(1)
+    
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    while True:
-        user_input = input("\nEnter your command:")
-        s.sendall(user_input.strip().encode())
-        data = s.recv(1024)
-        command = data.strip().decode()
-        if command == "endme":
-            break
+    root = tk.Tk()
+
+    sw = root.winfo_screenwidth()*5//6
+    sh = root.winfo_screenheight()*5//6
+    button_width = sw // 3
+    button_height= sh // 3
+
+    root.title("Reaper Midi Control Client")
+    root.geometry("%dx%d+0+0" % (sw, sh))
+    root.resizable(0,0)
+    root.config(bg="black")
+
+    title_frame = tk.Frame(root, bg="black", height=button_height)
+    title_frame.pack_propagate(0)
+    title_frame.pack(side=tk.TOP, fill=tk.X)
+
+    title_label = tk.Label(title_frame,bg="black",fg="red",text="Reaper Midi Control",font=("Comic Sans MS", (sh//12)))
+    title_label.pack(fill=tk.BOTH, expand=1)
+
+
+
+    quit_frame = tk.Frame(root, bg="black", width=button_width/2, height=button_height/2)
+    quit_frame.pack_propagate(0)
+    quit_frame.pack(expand=1)
+
+    button_frame = tk.Frame(root,bg="black")
+    button_frame.pack_propagate(0)
+    button_frame.pack(fill=tk.BOTH, expand=1)
+
+    stop_frame = tk.Frame(button_frame, width=button_width, height=button_height)
+    stop_frame.pack_propagate(0)
+    stop_frame.pack(side=tk.LEFT)
+
+    play_frame = tk.Frame(button_frame, width=button_width, height=button_height)
+    play_frame.pack_propagate(0)
+    play_frame.pack(side=tk.LEFT)
+
+    record_frame = tk.Frame(button_frame, width=button_width, height=button_height)
+    record_frame.pack_propagate(0)
+    record_frame.pack(side=tk.LEFT)
+
+
+    stop_button = tk.Button(stop_frame, text="Stop", font=("Comic Sans MS", (sh//16)), command=lambda x=s: stop_button_cmd(x))
+    play_button = tk.Button(play_frame, text="Play", font=("Comic Sans MS", (sh//16)), command=lambda x=s: play_button_cmd(x))
+    record_button = tk.Button(record_frame, text="Record", font=("Comic Sans MS", (sh//16)), command=lambda x=s: record_button_cmd(x))
+    quit_button = tk.Button(quit_frame, text="Quit", font=("Comic Sans MS", (sh//16)), command=lambda x=s: quit_button_cmd(x))
+
+    quit_button.pack(fill=tk.BOTH, expand=1)
+    stop_button.pack(fill=tk.BOTH, expand=1)
+    play_button.pack(fill=tk.BOTH, expand=1)
+    record_button.pack(fill=tk.BOTH, expand=1)
+    
+    root.mainloop()
+
+
+
+
+    #while True:
+    #    user_input = input("\nEnter your command:")
+    #    s.sendall(user_input.strip().encode())
+    #    data = s.recv(1024)
+    #    command = data.strip().decode()
+    #    if command == "endme":
+    #        break
 
 sys.exit(1)
---
+++ reaper_control_server.py
@@ -0,0 +1,105 @@
+# The goal is to write server software that listens for input from custom client software
+# to remotely control the DAW Reaper via Midi Signals
+
+# Helpful notes:
+# get_device_info(an_id) -> (interf, name, input, output, opened)
+# Midi Note 70 = Stop
+# Midi Note 72 = Play
+# Midi Note 74 = Record
+
+import pygame
+import pygame.midi
+import socket
+import time
+import sys
+
+HOST = ""
+PORT = 6666
+
+class ReaperControlServer:
+
+    def __init__(self):
+        #initialize midi
+        pygame.midi.init()
+        
+        self.port_input  = -1
+        self.port_output = -1
+        
+        #prints midi device info
+        for id in range(pygame.midi.get_count()):
+            test_port = pygame.midi.get_device_info(id)[1].decode("UTF-8")
+            if test_port == "KReichJr's Port":
+                if pygame.midi.get_device_info(id)[2] == 1:
+                    self.port_input = id
+                elif pygame.midi.get_device_info(id)[3] == 1:
+                    self.port_output = id
+                
+        if self.port_input < 0 or self.port_output < 0:
+            print("Missing required ports!")
+            sys.exit(1)
+
+        self.midi_out = pygame.midi.Output(self.port_output)
+        self.midi_in  = pygame.midi.Input(self.port_input)
+
+        self.midi_out.set_instrument(0)
+
+    def stopSong(self):
+        self.midi_out.note_on(70,127)
+        self.midi_out.note_off(70,127)
+    
+    def playSong(self):
+        self.midi_out.note_on(72,127)
+        self.midi_out.note_off(72,127)
+    
+    def recordSong(self):
+        self.midi_out.note_on(74,127)
+        self.midi_out.note_off(74,127)
+
+    def endMidi(self):
+        self.midi_in.close()
+        self.midi_out.close()
+        pygame.midi.quit()
+
+main_program = ReaperControlServer()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST,PORT))
+    print("\n\nListening for client connections...\n")
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print("Connected by", addr)
+        while True:
+            data = conn.recv(1024)
+            command = data.strip().decode()
+            print(command)
+            if command == "stop":
+                main_program.stopSong()
+            elif command == "play":
+                main_program.playSong()
+            elif command == "record":
+                main_program.recordSong()
+            elif command == "endme":
+                main_program.endMidi()
+                sys.exit(1)
+            else:
+                continue
+            if not data: break
+            conn.sendall(data)
+
+main_program.endMidi()
+sys.exit(1)
+
+#while True:
+#    command = input("\nEnter your command. Choices are 'stop' or 'play' or 'record' or 'endme': ")
+#    if command == "stop":
+#        main_program.stopSong()
+#    elif command == "play":
+#        main_program.playSong()
+#    elif command == "record":
+#        main_program.recordSong()
+#    elif command == "endme":
+#        main_program.endMidi()
+#        sys.exit(1)
+#    else:
+#        continue
